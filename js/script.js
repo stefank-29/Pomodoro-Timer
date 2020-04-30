2c02a34f3e7f69b03af79b5eed5bccbf6bfa9365
stefan <stefankaraferovic@gmail.com>
2020-05-01 01:38:15 +0200

    Html and some style

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let startTimer = false;
+let startBrake = false;
+let minutes = 25;
+let seconds = 60;
+let interval;
+
+const display = document.querySelector("#display");
+
+const play = document.querySelector("#play");
+const reset = document.querySelector("#reset");
+
+play.addEventListener('click', startCountdown);
+
+function startCountdown(timeInSeconds){
+    let startTime = Date.now() + (Number(timeInSeconds) * 1000);
+    setInterval( 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        ,1000)
+
+
+
+}
