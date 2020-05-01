928508c5b429963576c60f82d47d2990638e31d4
stefan <stefankaraferovic@gmail.com>
2020-05-01 12:23:32 +0200

    Countdown for timer

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ let startBrake = false;
 let minutes = 25;
 let seconds = 60;
 let interval;
-
+let relax = false;
+let work = true;
+let secondsCountdown;
 const display = document.querySelector("#display");
 
 const play = document.querySelector("#play");
@@ -12,24 +14,33 @@ const reset = document.querySelector("#reset");
 play.addEventListener('click', startCountdown);
 
 function startCountdown(timeInSeconds){
-    let startTime = Date.now() + (Number(timeInSeconds) * 1000);
-    setInterval( 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        ,1000)
+    const startTime = Date.now() + (Number(timeInSeconds) * 1000);
+    interval = setInterval(() => {
+        secondsCountdown = Math.round((startTime - Date.now()) / 1000);
+        if(secondsCountdown >= 0){
+            displayTime(secondsCountdown);
+            
 
+        }else{
+           if(work){
+               work = false;
+               relax = true;
+               startCountdown(300);
+           }else if(relax){
+               relax = false;
+               work = true;
+           }
 
+        }      
+        
+    }, 1000);
 
 }
+
+function displayTime(timeInSeconds){
+    let minutes = Math.floor(Number(timeInSeconds) / 60);
+    let seconds = Number(timeInSeconds) % 60;
+    display.textContent = `${minutes > 9 ? minutes : "0" +minutes}:${seconds > 9 ? seconds : "0" + seconds }`;
+}
+
+startCountdown(10);
