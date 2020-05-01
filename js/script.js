6082ca32185418609a17be5025838e71463142d5
stefan <stefankaraferovic@gmail.com>
2020-05-01 14:27:06 +0200

    Countdown error fixed

--- js/script.js
+++ js/script.js
@@ -1,35 +1,43 @@
-let startTimer = false;
-let startBrake = false;
-let minutes = 25;
-let seconds = 60;
 let interval;
+let secondsCountdown;
 let relax = false;
 let work = true;
-let secondsCountdown;
+
 const display = document.querySelector("#display");
 
 const play = document.querySelector("#play");
 const reset = document.querySelector("#reset");
 
-play.addEventListener('click', startCountdown);
+
+function startPomodoro(timeInSeconds){
+    startCountdown(timeInSeconds);
+}
+
+function startBreak(timeInSeconds){
+    startCountdown(timeInSeconds);
+}
 
 function startCountdown(timeInSeconds){
     const startTime = Date.now() + (Number(timeInSeconds) * 1000);
     interval = setInterval(() => {
         secondsCountdown = Math.round((startTime - Date.now()) / 1000);
-        if(secondsCountdown >= 0){
-            displayTime(secondsCountdown);
-            
+        if(secondsCountdown < 0){
+            clearInterval(interval);
+            secondsCountdown = 0;
+            if(work){
+                work = false;
+                relax = true;
+                secondsCountdown = 0;
+                startBreak(300);
+            }else if(relax){
+                relax = false;
+                work = true;
+                secondsCountdown = 0;
+                startPomodoro(1500);
+            }
 
         }else{
-           if(work){
-               work = false;
-               relax = true;
-               startCountdown(300);
-           }else if(relax){
-               relax = false;
-               work = true;
-           }
+            displayTime(secondsCountdown);
 
         }      
         
@@ -37,10 +45,14 @@ function startCountdown(timeInSeconds){
 
 }
 
+// displaying time on display
 function displayTime(timeInSeconds){
     let minutes = Math.floor(Number(timeInSeconds) / 60);
     let seconds = Number(timeInSeconds) % 60;
-    display.textContent = `${minutes > 9 ? minutes : "0" +minutes}:${seconds > 9 ? seconds : "0" + seconds }`;
+    display.textContent = `${minutes > 9 ? minutes : '0' +minutes}:${seconds > 9 ? seconds : '0' + seconds }`;
 }
 
-startCountdown(10);
+play.addEventListener('click', () => {
+    startCountdown(10);
+
+})
