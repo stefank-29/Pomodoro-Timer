f3ef4aeda9c39970abe620fa9a74d83e5ca82203
stefan <stefankaraferovic@gmail.com>
2020-05-02 01:33:42 +0200

    Play/Pause button

--- js/script.js
+++ js/script.js
@@ -2,18 +2,26 @@ let interval;
 let secondsCountdown;
 let relax = false;
 let work = true;
+let time;
+let start = false;
 
 const display = document.querySelector("#display");
+const state = document.querySelector("#state");
 
 const play = document.querySelector("#play");
 const reset = document.querySelector("#reset");
 
+const pomodoroTime = document.querySelector("#pomodoro-time");
+const breakTime = document.querySelector("#break-time");
+
 
 function startPomodoro(timeInSeconds){
+    displayTime(timeInSeconds);
     startCountdown(timeInSeconds);
 }
 
 function startBreak(timeInSeconds){
+    displayTime(timeInSeconds);
     startCountdown(timeInSeconds);
 }
 
@@ -27,18 +35,18 @@ function startCountdown(timeInSeconds){
             if(work){
                 work = false;
                 relax = true;
-                secondsCountdown = 0;
-                startBreak(300);
+                time = Number(breakTime.textContent) * 60;
+                state.textContent = "Relaxing"
+                startBreak(time);
             }else if(relax){
                 relax = false;
                 work = true;
-                secondsCountdown = 0;
-                startPomodoro(1500);
+                time = Number(pomodoroTime.textContent) * 60;
+                state.textContent = "Working";
+                startPomodoro(time);
             }
-
         }else{
             displayTime(secondsCountdown);
-
         }      
         
     }, 1000);
@@ -49,10 +57,28 @@ function startCountdown(timeInSeconds){
 function displayTime(timeInSeconds){
     let minutes = Math.floor(Number(timeInSeconds) / 60);
     let seconds = Number(timeInSeconds) % 60;
-    display.textContent = `${minutes > 9 ? minutes : '0' +minutes}:${seconds > 9 ? seconds : '0' + seconds }`;
+    display.textContent = `${minutes > 9 ? minutes : '0' + minutes}:${seconds > 9 ? seconds : '0' + seconds }`;
+    
 }
 
 play.addEventListener('click', () => {
-    startCountdown(10);
+    if(work){
+        if(!start){
+            start = true;
+            play.textContent = "Pause";
+            time = Number(pomodoroTime.textContent) * 60;
+            startPomodoro(secondsCountdown);
+        }else{
+            start = false;
+            play.textContent = "Start";
+            clearInterval(interval);
+            interval = -1;
+
+        }
+
+    }else if(relax){
+        time = Number(breakTime.textContent) * 60;
+        startBreak(time);
+    }
 
 })
