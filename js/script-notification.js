66000c29eb3b9f124c58815b56cf6252f55559cc
stefan <stefankaraferovic@gmail.com>
2020-05-05 22:01:31 +0200

    Notifications properties

---
+++ js/script-notification.js
@@ -0,0 +1,41 @@
+/*Notification.requestPermission().then(function(result) {
+    console.log(result);
+  });
+
+  function spawnNotification(body, icon, title) {
+    let options = {
+        body: body,
+        icon: icon
+    };
+    let n = new Notification(title, options);
+}*/
+
+function notifyMe(body, icon, title) {   // provera da li browser podrzava notifikacije i da li je dao dozvolu
+    if (!("Notification" in window)) {
+      alert("This browser does not support system notifications");
+    }
+    else if (Notification.permission === "granted") {
+      notify(body, icon, title);
+    }
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        if (permission === "granted") {
+          notify(body, icon, title);
+        }
+      });
+    }
+}
+    
+    function notify(body, icon, title) {
+        let options = {
+            body: body,
+            icon: icon,
+        };
+        
+        let notification = new Notification(title, options);
+        notification.onclick = function () {
+            window.open("http://carnes.cc");      
+        };
+        setTimeout(notification.close.bind(notification), 7000); 
+    }
+  
--- js/script.js
+++ js/script.js
@@ -68,6 +68,7 @@ function startCountdown(seconds){
             secondsCountdown = 0;
             if(work){
                 alarm.play();
+                notifyMe("It's time for a break", "images/timer-icon.png", "Pomodoro timer");
                 displaySpentTime(pomodoroTime.textContent); // prikaz proteklog ucenja
                 work = false;
                 relax = true;
@@ -78,6 +79,7 @@ function startCountdown(seconds){
                 startBreak(time);
             }else if(relax){
                 alarm.play();
+                notifyMe("It's time for work", "images/timer-icon", "Pomodoro timer");
                 displaySpentTime(breakTime.textContent); // ukupno odmora
                 relax = false;
                 work = true;
@@ -291,3 +293,4 @@ breakUp.addEventListener('click', () =>{
     }
     
 });
+
